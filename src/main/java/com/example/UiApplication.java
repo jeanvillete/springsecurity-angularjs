33c36459568ea17321a4280f2b68ecb2dd4cca8f
Jean Villete <jean.villete@gmail.com>
2016-08-02 19:36:15 -0300

    changing the backend url for the 'resource' @RequestMapping

--- src/main/java/com/example/UiApplication.java
+++ src/main/java/com/example/UiApplication.java
@@ -20,14 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UiApplication {
 
-	@RequestMapping( "/resource" )
-	public Map<String, Object> home() {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put( "id", UUID.randomUUID().toString() );
-		model.put( "content", "Hello World" );
-		return model;
-	}
-	
 	/* This is a useful trick in a Spring Security application. 
 	 * If the "/user" resource is reachable then it will return the currently authenticated 
 	 * user (an Authentication), and otherwise Spring Security will intercept the request 
